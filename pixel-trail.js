e9a73c7fc16b017415b0442c2bc8e9455f589b34
McKathlin <mckathlin@gmail.com>
2024-10-30 15:26:58 +0000

    Make pixels turn on when hovered

--- pixel-trail.js
+++ pixel-trail.js
@@ -9,19 +9,33 @@ PixelTrail.defaultColumnCount = 16;
 PixelTrail.gridContainer = null;
 
 //=============================================================================
-// Setup and Reset
+// Setup
 //=============================================================================
 
 document.addEventListener("DOMContentLoaded", (event) => {
     PixelTrail.gridContainer = document.querySelector("#pixel-container");
+
     PixelTrail.resetGrid(
         PixelTrail.defaultColumnCount,
         PixelTrail.defaultRowCount
     );
+
+    PixelTrail.gridContainer.addEventListener("mouseover", (event) => {
+        if (event.target.classList.contains("pixel")) {
+            PixelTrail.turnOnPixel(event.target);
+        }
+    })
 })
 
+//=============================================================================
+// Grid Logic
+//=============================================================================
+
 PixelTrail.resetGrid = function(columnCount, rowCount) {
+    // empty grid
     PixelTrail.gridContainer.replaceChildren();
+
+    // repopulate grid
     for (let row = 0; row < rowCount; row++) {
         for (let col = 0; col < columnCount; col++) {
             let pixelNode = document.createElement("div");
@@ -29,9 +43,8 @@ PixelTrail.resetGrid = function(columnCount, rowCount) {
             PixelTrail.gridContainer.appendChild(pixelNode);
         }
     }
-}
+};
 
-//=============================================================================
-// Pixel Interaction
-//=============================================================================
-
+PixelTrail.turnOnPixel = function(pixelNode) {
+    pixelNode.classList.add("on");
+};
